5fe7ff082fd46214d84bbb3508976e58e358c0f9
Archippa <kallesaretsalo@gmail.com>
2025-08-14 03:19:47 +0300

    added Player.containers and modified the game loop to use them

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
     print("Starting Asteroids!")
